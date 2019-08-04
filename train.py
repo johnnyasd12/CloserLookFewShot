72ed798698f1dc95e61c249c2b03b3109af9aae2
johnnyasd12 <johnnnyasd12@gmail.com>
2019-08-04 18:13:53 +0800

    print best acc

--- train.py
+++ train.py
@@ -47,7 +47,8 @@ def train(base_loader, val_loader, model, optimization, start_epoch, stop_epoch,
         if (epoch % params.save_freq==0) or (epoch==stop_epoch-1):
             outfile = os.path.join(params.checkpoint_dir, '{:d}.tar'.format(epoch))
             torch.save({'epoch':epoch, 'state':model.state_dict()}, outfile)
-
+    print('The best accuracy is',max_acc)
+    
     return model
 
 if __name__=='__main__':
