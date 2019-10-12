9ba5dd213e27eb5f411f20f3bb19951dd3b84292
johnnyasd12 <johnnnyasd12@gmail.com>
2019-10-12 10:06:34 +0800

    add run_exp.sh

---
+++ run_exp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+dataset=$1
+# export dataset
+# echo $dataset
+# python ./yee.py --dataset $dataset
+
+python ./train.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 0.1 --stop_epoch 500
+python ./save_features.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 0.1
+python ./test.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 0.1
+
+python ./train.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 1 --stop_epoch 500
+python ./save_features.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 1
+python ./test.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 1
+
+python ./train.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 10.0 --stop_epoch 500
+python ./save_features.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 10.0
+python ./test.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 10.0
+
+python ./train.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder HiddenConv --recons_lambda 0.1 --stop_epoch 500
+python ./save_features.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder HiddenConv --recons_lambda 0.1
+python ./test.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder HiddenConv --recons_lambda 0.1
+
+python ./train.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder HiddenConv --recons_lambda 1 --stop_epoch 500
+python ./save_features.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder HiddenConv --recons_lambda 1
+python ./test.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder HiddenConv --recons_lambda 1
+
+python ./train.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder HiddenConv --recons_lambda 10.0 --stop_epoch 500
+python ./save_features.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder HiddenConv --recons_lambda 10.0
+python ./test.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder HiddenConv --recons_lambda 10.0
+
+python ./train.py --dataset $dataset --model Conv4 --method protonet --train_aug
+python ./save_features.py --dataset $dataset --model Conv4 --method protonet --train_aug
+python ./test.py --dataset $dataset --model Conv4 --method protonet --train_aug
