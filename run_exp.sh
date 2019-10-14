7d6b4952cff89c84354701833ada2b6bc36ca227
johnnyasd12 <johnnnyasd12@gmail.com>
2019-10-14 17:50:25 +0800

    just change the order

--- run_exp.sh
+++ run_exp.sh
@@ -5,6 +5,10 @@ dataset=$1
 # echo $dataset
 # python ./yee.py --dataset $dataset
 
+python ./train.py --dataset $dataset --model Conv4 --method protonet --train_aug
+python ./save_features.py --dataset $dataset --model Conv4 --method protonet --train_aug
+python ./test.py --dataset $dataset --model Conv4 --method protonet --train_aug
+
 python ./train.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 0.1 --stop_epoch 500
 python ./save_features.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 0.1
 python ./test.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder Conv --recons_lambda 0.1
@@ -29,6 +33,3 @@ python ./train.py --dataset $dataset --model Conv4 --method protonet --train_aug
 python ./save_features.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder HiddenConv --recons_lambda 10.0
 python ./test.py --dataset $dataset --model Conv4 --method protonet --train_aug --recons_decoder HiddenConv --recons_lambda 10.0
 
-python ./train.py --dataset $dataset --model Conv4 --method protonet --train_aug
-python ./save_features.py --dataset $dataset --model Conv4 --method protonet --train_aug
-python ./test.py --dataset $dataset --model Conv4 --method protonet --train_aug
