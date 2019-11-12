9f0cb7be71e5f53592104e410617cfba756673dc
johnnyasd12 <johnnnyasd12@gmail.com>
2019-11-12 18:46:08 +0800

    add Lr-LiVAE repository dir

--- configs.py
+++ configs.py
@@ -6,3 +6,5 @@ data_dir['omniglot']        = './filelists/omniglot/'
 data_dir['emnist']          = './filelists/emnist/' 
 
 gpu_id                      = '1'
+
+llvae_dir                   = '/media/deep3072/Data/tu/disentangled/Lr-LiVAE/'
