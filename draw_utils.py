8b02775583af4dfe4cd44c31a3954f6c02136c67
johnnyasd12 <johnnnyasd12@gmail.com>
2020-03-06 00:17:50 +0800

    init draw_utils

--- draw_utils.py
+++ draw_utils.py
@@ -1,3 +1,49 @@
+import os
+import pandas as pd
+from matplotlib import pyplot as plt
+
+class ExpPlotter:
+    def __init__(self, csv_name, record_folder, negligible_vars, dependent_vars):
+        self.csv_path = os.path.join(record_folder, csv_name)
+        self.df = pd.read_csv(self.csv_path)
+        self.negligible_vars = negligible_vars
+        self.dependent_vars = dependent_vars
+        
+        self.df_drop = ExpPlotter.drop_duplicate(self.df)
+        self.controllable_vars = [x for x in list(self.df_drop.columns) if x not in negligible_vars and x not in dependent_vars]
+        
+        
+        
+    def plot_exps(self, independent_var, dependent_var):
+        control_vars = self.controllable_vars.copy()
+        control_vars.remove(independent_var)
+#         print('control_vars:', control_vars)
+        
+        all_settings_df = self.df_drop[control_vars].drop_duplicates()
+
+        for _, row in all_settings_df.iterrows():
+            sub_df = self.df_drop.copy()
+
+            print('Control Variables:')
+            for k,v in row.items():
+                print(k, ':', v)
+                if v==v: # v is not nan
+                    sub_df = sub_df[sub_df[k]==v]
+                else: # v is nan
+                    sub_df = sub_df[sub_df[k]!=sub_df[k]]
+
+            print(sub_df)
+#             plt.hist()
+    
+    
+    def drop_duplicate(df):
+        # drop columns with all the same row values
+        nunique = df.apply(pd.Series.nunique)
+        cols_to_drop = nunique[nunique == 1].index
+        df_drop = df.drop(cols_to_drop, axis=1)
+        # drop columns with all NAN values
+        df_drop = df_drop.dropna(axis=1, how='all')
+        return df_drop
 
 
 
