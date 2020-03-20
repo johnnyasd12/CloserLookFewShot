64eff1036e038415d108f673f5139e9e38e5d632
johnnyasd12 <johnnnyasd12@gmail.com>
2020-03-20 20:10:55 +0800

    delete useless code

--- methods/protonet.py
+++ methods/protonet.py
@@ -19,29 +19,11 @@ class ProtoNet(MetaTemplate):
     def set_forward(self,x,is_feature = False):
         ''' get the last output (scores of query set) from image or embedding
         '''
-#         try:
         z_support, z_query  = self.parse_feature(x,is_feature)
-#         z_query_cp = z_query # TODO: delete this line
-
         z_support   = z_support.contiguous()
         z_proto     = z_support.view(self.n_way, self.n_support, -1 ).mean(1) #the shape of z is [n_data, n_dim]
         z_query     = z_query.contiguous().view(self.n_way* self.n_query, -1 )
-
-
         dists = euclidean_dist(z_query, z_proto)
-#             if np.random.random() < 0.1:
-#                 print('x.size():', x.size())
-#                 print('z_support.size():', z_support.size())
-#                 print('z_query_cp.size():', z_query_cp.size())
-#                 print('z_proto.size():', z_proto.size())
-#                 print('z_query.size():', z_query.size())
-#         except AssertionError:
-#             print('AssertionError occurred!!')
-#             print('x.size():', x.size())
-#             print('z_support.size():', z_support.size())
-#             print('z_query_cp.size():', z_query_cp.size()) # problem is z_query_cp.size(1) should be n_way*n_query
-#             print('z_proto.size():', z_proto.size())
-#             print('z_query.size():', z_query.size())
         scores = -dists
         return scores
 
@@ -49,15 +31,11 @@ class ProtoNet(MetaTemplate):
         ''' compute task loss (by query set) given support and query set
         '''
         y_query = torch.from_numpy(np.repeat(range( self.n_way ), self.n_query ))
-#         y_query = Variable(y_query.cuda())
-        if self.device is None:
+#         if self.device is None:
 #             y_query = Variable(to_device(y_query))
-            y_query = Variable(y_query.cuda())
-        else:
-            y_query = Variable(y_query.cuda())
-
+#         else:
+        y_query = Variable(y_query.cuda())
         scores = self.set_forward(x)
-
         return self.loss_fn(scores, y_query )
     
     def total_loss(self, x):
