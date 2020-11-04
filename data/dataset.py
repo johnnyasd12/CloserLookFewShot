09ca7db37b9146b0d451e39d17b68f8362caf5ea
johnnyasd12 <johnnnyasd12@gmail.com>
2020-11-04 18:54:39 +0800

    reorder dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -12,6 +12,120 @@ import h5py
 from my_utils import *
 
 
+
+
+class SetDataset:
+    def __init__(self, data_file, batch_size, transform): # one item = ONE specific Class, but batch_sampler call this several times at once
+        with open(data_file, 'r') as f: # data_file is json
+            self.meta = json.load(f)
+
+        self.cl_list = np.unique(self.meta['image_labels']).tolist()
+
+        self.sub_meta = {} # ALL images, content = class_1: [data_path_1, ..., data_path_n], class_k: [data_path_1, ..., ]
+        for cl in self.cl_list:
+            self.sub_meta[cl] = []
+
+        for x,y in zip(self.meta['image_names'],self.meta['image_labels']):
+            self.sub_meta[y].append(x)
+
+        self.sub_dataloaders = [] # there're k dataloaders, k is # classes
+        sub_data_loader_params = dict(batch_size = batch_size, # how many data to grab at current category???
+                                  shuffle = True,
+                                  num_workers = 0, #use main thread only or may receive multiple batches
+                                  pin_memory = False)        
+        for cl in self.cl_list:
+            sub_dataset = SubDataset(self.sub_meta[cl], cl, transform = transform )
+            self.sub_dataloaders.append( torch.utils.data.DataLoader(sub_dataset, **sub_data_loader_params) )
+
+    def __getitem__(self,i):
+        return next(iter(self.sub_dataloaders[i]))
+
+    def __len__(self):
+        return len(self.cl_list)
+
+
+class SubDataset: # one iteration is one image of one class
+    def __init__(self, sub_meta, cl, transform=transforms.ToTensor(), target_transform=identity):
+        self.sub_meta = sub_meta # list of image names(dirs)
+        self.cl = cl 
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __getitem__(self,i):
+        #print( '%d -%d' %(self.cl,i))
+        image_path = os.path.join( self.sub_meta[i])
+        img = Image.open(image_path).convert('RGB')
+        img = self.transform(img)
+        target = self.target_transform(self.cl)
+        return img, target
+
+    def __len__(self):
+        return len(self.sub_meta)
+
+class HDF5Dataset:
+    def __init__(self, data_file, transform, target_transform=identity):
+        self.file_path = data_file
+        self.imgs = None
+        self.labels = None
+        with h5py.File(self.file_path, 'r') as file:
+            self.dataset_len = len(file["labels"])
+        
+#         self.file = h5py.File(data_file, 'r')
+        self.transform = transform
+        self.target_transform = target_transform
+        
+    def __getitem__(self, i):
+        if self.imgs is None:
+            self.imgs = h5py.File(self.file_path, 'r')["images"]
+            self.labels = h5py.File(self.file_path, 'r')["labels"]
+        img = self.imgs[i]
+        target = self.labels[i]
+#         img = self.file['images'][i,:,:,:]
+#         target = self.file['labels'][i]
+        target = self.target_transform(target)
+        return img, target
+        
+    def __len__(self):
+#         return self.file['labels'].shape[0]
+        return self.dataset_len
+            
+
+class SimpleDataset:
+    def __init__(self, data_file, transform, target_transform=identity, return_path=False):
+        with open(data_file, 'r') as f:
+            self.meta = json.load(f)
+        self.transform = transform
+        self.target_transform = target_transform
+        self.return_path = return_path
+
+    def __getitem__(self,i):
+        image_path = self.meta['image_names'][i]
+#         image_path = os.path.join(???, image_path)
+        img = Image.open(image_path).convert('RGB')
+        img = self.transform(img)
+        target = self.target_transform(self.meta['image_labels'][i])
+        if self.return_path:
+            return image_path, img, target
+        else:
+            return img, target
+
+    def __len__(self):
+        return len(self.meta['image_names'])
+
+class EpisodicBatchSampler(object): # to sample n_way classes index for every iteration
+    def __init__(self, n_classes, n_way, n_episodes):
+        self.n_classes = n_classes
+        self.n_way = n_way
+        self.n_episodes = n_episodes
+
+    def __len__(self):
+        return self.n_episodes
+
+    def __iter__(self):
+        for i in range(self.n_episodes):
+            yield torch.randperm(self.n_classes)[:self.n_way]
+
+
 # class VAESetDataset:
 #     def __init__(self, data_file, batch_size, pre_transform, post_transform, aug_transform): # only ONE Class for one item, but batch_sampler call this several times at once
 #         with open(data_file, 'r') as f: # data_file is json
@@ -47,6 +161,7 @@ from my_utils import *
 #     def __len__(self):
 #         return len(self.cl_list)
         
+
 class VAESetDataset:
     def __init__(self, data_file, batch_size, transform, fake_img_transform, vaegan_params): # only ONE Class for one item, but batch_sampler call this several times at once
 #         self.vaegan_exp = vaegan_exp
@@ -208,35 +323,6 @@ class AugSimpleDataset:
         self.debug_flag += 1
 #         print('self.debug_flag =', self.debug_flag)
 
-class SetDataset:
-    def __init__(self, data_file, batch_size, transform): # one item = ONE specific Class, but batch_sampler call this several times at once
-        with open(data_file, 'r') as f: # data_file is json
-            self.meta = json.load(f)
-
-        self.cl_list = np.unique(self.meta['image_labels']).tolist()
-
-        self.sub_meta = {} # ALL images, content = class_1: [data_path_1, ..., data_path_n], class_k: [data_path_1, ..., ]
-        for cl in self.cl_list:
-            self.sub_meta[cl] = []
-
-        for x,y in zip(self.meta['image_names'],self.meta['image_labels']):
-            self.sub_meta[y].append(x)
-
-        self.sub_dataloaders = [] # there're k dataloaders, k is # classes
-        sub_data_loader_params = dict(batch_size = batch_size, # how many data to grab at current category???
-                                  shuffle = True,
-                                  num_workers = 0, #use main thread only or may receive multiple batches
-                                  pin_memory = False)        
-        for cl in self.cl_list:
-            sub_dataset = SubDataset(self.sub_meta[cl], cl, transform = transform )
-            self.sub_dataloaders.append( torch.utils.data.DataLoader(sub_dataset, **sub_data_loader_params) )
-
-    def __getitem__(self,i):
-        return next(iter(self.sub_dataloaders[i]))
-
-    def __len__(self):
-        return len(self.cl_list)
-
 class VAESubDataset: # one iteration is one image of one class
     def __init__(self, sub_meta, cl, transform=transforms.ToTensor(), fake_img_transform=None, vaegan_params=None, target_transform=identity):
         '''
@@ -292,83 +378,3 @@ class VAESubDataset: # one iteration is one image of one class
     def __len__(self):
         return len(self.sub_meta)
 
-class SubDataset: # one iteration is one image of one class
-    def __init__(self, sub_meta, cl, transform=transforms.ToTensor(), target_transform=identity):
-        self.sub_meta = sub_meta # list of image names(dirs)
-        self.cl = cl 
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __getitem__(self,i):
-        #print( '%d -%d' %(self.cl,i))
-        image_path = os.path.join( self.sub_meta[i])
-        img = Image.open(image_path).convert('RGB')
-        img = self.transform(img)
-        target = self.target_transform(self.cl)
-        return img, target
-
-    def __len__(self):
-        return len(self.sub_meta)
-
-class HDF5Dataset:
-    def __init__(self, data_file, transform, target_transform=identity):
-        self.file_path = data_file
-        self.imgs = None
-        self.labels = None
-        with h5py.File(self.file_path, 'r') as file:
-            self.dataset_len = len(file["labels"])
-        
-#         self.file = h5py.File(data_file, 'r')
-        self.transform = transform
-        self.target_transform = target_transform
-        
-    def __getitem__(self, i):
-        if self.imgs is None:
-            self.imgs = h5py.File(self.file_path, 'r')["images"]
-            self.labels = h5py.File(self.file_path, 'r')["labels"]
-        img = self.imgs[i]
-        target = self.labels[i]
-#         img = self.file['images'][i,:,:,:]
-#         target = self.file['labels'][i]
-        target = self.target_transform(target)
-        return img, target
-        
-    def __len__(self):
-#         return self.file['labels'].shape[0]
-        return self.dataset_len
-            
-
-class SimpleDataset:
-    def __init__(self, data_file, transform, target_transform=identity, return_path=False):
-        with open(data_file, 'r') as f:
-            self.meta = json.load(f)
-        self.transform = transform
-        self.target_transform = target_transform
-        self.return_path = return_path
-
-    def __getitem__(self,i):
-        image_path = self.meta['image_names'][i]
-#         image_path = os.path.join(???, image_path)
-        img = Image.open(image_path).convert('RGB')
-        img = self.transform(img)
-        target = self.target_transform(self.meta['image_labels'][i])
-        if self.return_path:
-            return image_path, img, target
-        else:
-            return img, target
-
-    def __len__(self):
-        return len(self.meta['image_names'])
-
-class EpisodicBatchSampler(object): # to sample n_way classes index for every iteration
-    def __init__(self, n_classes, n_way, n_episodes):
-        self.n_classes = n_classes
-        self.n_way = n_way
-        self.n_episodes = n_episodes
-
-    def __len__(self):
-        return self.n_episodes
-
-    def __iter__(self):
-        for i in range(self.n_episodes):
-            yield torch.randperm(self.n_classes)[:self.n_way]
